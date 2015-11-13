3b08ff9ab65dccc9f189dae85e6decf309bfe874
Roman Sahel <romsahel@gmail.com>
2015-11-13 11:05:36 +0100

    Put the generation code into a clas

--- src/pacman/entries/pacman/behaviortree/MyPacMan.java
+++ src/pacman/entries/pacman/behaviortree/MyPacMan.java
@@ -1,43 +1,14 @@
 package pacman.entries.pacman.behaviortree;
 
-import java.util.Random;
 import pacman.controllers.Controller;
 import pacman.entries.pacman.GameState;
 import pacman.entries.pacman.behaviortree.helpers.Composite;
-import pacman.entries.pacman.behaviortree.helpers.Inverter;
-import pacman.entries.pacman.behaviortree.helpers.Leaf;
-import pacman.entries.pacman.behaviortree.helpers.Node;
-import pacman.entries.pacman.behaviortree.helpers.Selector;
-import pacman.entries.pacman.behaviortree.helpers.Sequence;
-import pacman.entries.pacman.behaviortree.tasks.actions.AvoidPowerpillTask;
-import pacman.entries.pacman.behaviortree.tasks.actions.ChasePowerPillTask;
-import pacman.entries.pacman.behaviortree.tasks.actions.ChaseTask;
-import pacman.entries.pacman.behaviortree.tasks.actions.EatPillTask;
-import pacman.entries.pacman.behaviortree.tasks.actions.GoToJunctionTask;
-import pacman.entries.pacman.behaviortree.tasks.actions.RunAwayMultipleTask;
-import pacman.entries.pacman.behaviortree.tasks.actions.RunAwayTask;
-import pacman.entries.pacman.behaviortree.tasks.conditions.AreGhostFarAwayTask;
-import pacman.entries.pacman.behaviortree.tasks.conditions.IsGhostCloserTask;
-import pacman.entries.pacman.behaviortree.tasks.conditions.IsGhostEdibleTask;
-import pacman.entries.pacman.behaviortree.tasks.conditions.IsGhostNearTask;
-import pacman.entries.pacman.behaviortree.tasks.conditions.IsPacmanAtJunction;
-import pacman.entries.pacman.behaviortree.tasks.conditions.IsPathToJunctionSafeTask;
-import pacman.entries.pacman.behaviortree.tasks.conditions.IsPathToPowerPillSafeTask;
 import pacman.game.Constants.MOVE;
 import pacman.game.Game;
 
 public class MyPacMan extends Controller<MOVE>
 {
 
-	private static final int MAX_DEPTH = 4;
-	private static final int MAX_CHILDREN = 3;
-	private static final int LEAF_TYPE = 0;
-	private static final int SELECTOR_TYPE = 1;
-	private static final int SEQUENCE_TYPE = 2;
-
-	private static Leaf[] setOfActions, setOfConditions;
-
-	private final Random random;
 
 	/*
 	 * Root of the behavior tree
@@ -50,97 +21,14 @@ public class MyPacMan extends Controller<MOVE>
 	 * Type of MOVE that will be returned at each game step
 	 */
 	private MOVE move;
+	private final TreeGenerator treeGenerator;
 
 	public MyPacMan()
 	{
 		state = new GameState();
-		rootNode = new Selector();
-		rootNode.setMaxDepth(MAX_DEPTH);
-		random = new Random();
 
-		setOfActions = new Leaf[]
-		{
-			new AvoidPowerpillTask(this),
-			new ChasePowerPillTask(this),
-			new ChaseTask(this),
-			new EatPillTask(this),
-			new GoToJunctionTask(this),
-			new RunAwayMultipleTask(this),
-			new RunAwayTask(this)
-		};
-		setOfConditions = new Leaf[]
-		{
-			new AreGhostFarAwayTask(this),
-			new IsGhostCloserTask(this),
-			new IsGhostEdibleTask(this),
-			new IsGhostNearTask(this),
-			new IsPacmanAtJunction(this),
-			new IsPathToJunctionSafeTask(this),
-			new IsPathToPowerPillSafeTask(this)
-		};
-
-		generateChildren(rootNode);
-		TreePrinter.getInstance().print(rootNode, setOfConditions);
-	}
-
-	private Node generate(Composite root)
-	{
-		int type;
-		if (root.getDepth() >= root.getMaxDepth())
-			type = LEAF_TYPE;
-		else
-			type = random.nextInt(SEQUENCE_TYPE + 1);
-
-		if (type == LEAF_TYPE)
-			if (root.getChildrenCount() == root.nbChildren - 1)
-				return setOfActions[random.nextInt(setOfActions.length)];
-			else
-				return generateCondition(root);
-		else
-			return generateComposite(type, root);
-	}
-
-	private Node generateComposite(int type, Composite root)
-	{
-		Composite node;
-		if (type == SELECTOR_TYPE)
-			node = new Selector();
-		else
-			node = new Sequence();
-		node.setDepth(root.getDepth() + 1);
-		node.setMaxDepth(random.nextInt(MAX_DEPTH + 1));
-		generateChildren(node);
-		return node;
-	}
-
-	private Node generateCondition(Composite root)
-	{
-		Leaf leaf = null;
-		do
-		{
-			leaf = setOfConditions[random.nextInt(setOfConditions.length)];
-		} while (rootHasNode(root, leaf));
-
-		if (random.nextInt(100) < 10)
-			return new Inverter(leaf);
-		return leaf;
-	}
-
-	private void generateChildren(Composite node)
-	{
-		node.nbChildren = random.nextInt(MAX_CHILDREN + 1 - 2) + 2;
-		for (int i = 0; i < node.nbChildren; i++)
-			node.addChildren(generate(node));
-	}
-
-	private boolean rootHasNode(Composite root, Leaf leaf)
-	{
-		for (Node n : root.getChildren())
-			if (n.getClass() == leaf.getClass()
-				|| ((n instanceof Inverter)
-					&& ((Inverter) n).getInvertedNode().getClass() == leaf.getClass()))
-				return true;
-		return false;
+		treeGenerator = new TreeGenerator(this);
+		rootNode = treeGenerator.generate(true);
 	}
 
 	@Override
---
+++ src/pacman/entries/pacman/behaviortree/TreeGenerator.java
@@ -0,0 +1,163 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pacman.entries.pacman.behaviortree;
+
+import java.util.Random;
+import pacman.entries.pacman.behaviortree.helpers.Composite;
+import pacman.entries.pacman.behaviortree.helpers.Inverter;
+import pacman.entries.pacman.behaviortree.helpers.Leaf;
+import pacman.entries.pacman.behaviortree.helpers.Node;
+import pacman.entries.pacman.behaviortree.helpers.Selector;
+import pacman.entries.pacman.behaviortree.helpers.Sequence;
+import pacman.entries.pacman.behaviortree.tasks.actions.AvoidPowerpillTask;
+import pacman.entries.pacman.behaviortree.tasks.actions.ChasePowerPillTask;
+import pacman.entries.pacman.behaviortree.tasks.actions.ChaseTask;
+import pacman.entries.pacman.behaviortree.tasks.actions.EatPillTask;
+import pacman.entries.pacman.behaviortree.tasks.actions.GoToJunctionTask;
+import pacman.entries.pacman.behaviortree.tasks.actions.RunAwayMultipleTask;
+import pacman.entries.pacman.behaviortree.tasks.actions.RunAwayTask;
+import pacman.entries.pacman.behaviortree.tasks.conditions.AreGhostFarAwayTask;
+import pacman.entries.pacman.behaviortree.tasks.conditions.IsGhostCloserTask;
+import pacman.entries.pacman.behaviortree.tasks.conditions.IsGhostEdibleTask;
+import pacman.entries.pacman.behaviortree.tasks.conditions.IsGhostNearTask;
+import pacman.entries.pacman.behaviortree.tasks.conditions.IsPacmanAtJunction;
+import pacman.entries.pacman.behaviortree.tasks.conditions.IsPathToJunctionSafeTask;
+import pacman.entries.pacman.behaviortree.tasks.conditions.IsPathToPowerPillSafeTask;
+
+/**
+ *
+ * @author Romsahel
+ */
+public class TreeGenerator
+{
+
+	/**
+	 * Parameters
+	 */
+	private static final int MAX_DEPTH = 4;
+	private static final int MAX_CHILDREN = 3;
+
+	/**
+	 *
+	 */
+	private static final int LEAF_TYPE = 0;
+	private static final int SELECTOR_TYPE = 1;
+	private static final int SEQUENCE_TYPE = 2;
+
+	private Composite rootNode;
+	private final Leaf[] setOfActions, setOfConditions;
+
+	private static final Random random = new Random();
+
+	public TreeGenerator(MyPacMan thisPacman)
+	{
+		setOfActions = new Leaf[]
+		{
+			new AvoidPowerpillTask(thisPacman),
+			new ChasePowerPillTask(thisPacman),
+			new ChaseTask(thisPacman),
+			new EatPillTask(thisPacman),
+			new GoToJunctionTask(thisPacman),
+			new RunAwayMultipleTask(thisPacman),
+			new RunAwayTask(thisPacman)
+		};
+		setOfConditions = new Leaf[]
+		{
+			new AreGhostFarAwayTask(thisPacman),
+			new IsGhostCloserTask(thisPacman),
+			new IsGhostEdibleTask(thisPacman),
+			new IsGhostNearTask(thisPacman),
+			new IsPacmanAtJunction(thisPacman),
+			new IsPathToJunctionSafeTask(thisPacman),
+			new IsPathToPowerPillSafeTask(thisPacman)
+		};
+	}
+
+	public Composite generate()
+	{
+		return generate(false);
+	}
+
+	public Composite generate(boolean print)
+	{
+		rootNode = new Selector();
+		rootNode.setMaxDepth(MAX_DEPTH);
+		generateChildren(rootNode);
+		if (print)
+			this.print();
+		return rootNode;
+	}
+
+	private Node generate(Composite root)
+	{
+		int type;
+		if (root.getDepth() >= root.getMaxDepth())
+			type = LEAF_TYPE;
+		else
+			type = random.nextInt(SEQUENCE_TYPE + 1);
+
+		if (type == LEAF_TYPE)
+			if (root.getChildrenCount() == root.nbChildren - 1)
+				return setOfActions[random.nextInt(setOfActions.length)];
+			else
+				return generateCondition(root);
+		else
+			return generateComposite(type, root);
+	}
+
+	private Node generateComposite(int type, Composite root)
+	{
+		Composite node;
+		if (type == SELECTOR_TYPE)
+			node = new Selector();
+		else
+			node = new Sequence();
+		node.setDepth(root.getDepth() + 1);
+		node.setMaxDepth(random.nextInt(MAX_DEPTH + 1));
+		generateChildren(node);
+		return node;
+	}
+
+	private Node generateCondition(Composite root)
+	{
+		Leaf leaf = null;
+		do
+		{
+			leaf = setOfConditions[random.nextInt(setOfConditions.length)];
+		} while (rootHasNode(root, leaf));
+
+		if (random.nextInt(100) < 10)
+			return new Inverter(leaf);
+		return leaf;
+	}
+
+	private void generateChildren(Composite node)
+	{
+		node.nbChildren = random.nextInt(MAX_CHILDREN + 1 - 2) + 2;
+		for (int i = 0; i < node.nbChildren; i++)
+			node.addChildren(generate(node));
+	}
+
+	private boolean rootHasNode(Composite root, Leaf leaf)
+	{
+		for (Node n : root.getChildren())
+			if (n.getClass() == leaf.getClass()
+				|| ((n instanceof Inverter)
+					&& ((Inverter) n).getInvertedNode().getClass() == leaf.getClass()))
+				return true;
+		return false;
+	}
+
+	public void print()
+	{
+		TreePrinter.getInstance().print(rootNode, setOfConditions);
+	}
+
+	public Composite getRoot()
+	{
+		return rootNode;
+	}
+}
