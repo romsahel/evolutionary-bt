0839ab0154107b27032a71a64c582bc99c9c695a
Roman Sahel <romsahel@gmail.com>
2015-11-13 12:52:05 +0100

    Fix ChasePowerPill

--- src/pacman/entries/pacman/behaviortree/tasks/actions/ChasePowerPillTask.java
+++ src/pacman/entries/pacman/behaviortree/tasks/actions/ChasePowerPillTask.java
@@ -7,24 +7,28 @@ import pacman.game.Game;
 
 /**
  * Action task: sets the next move to go to the nearest powerpill
+ * <p>
  * @author romsahel
  */
 public class ChasePowerPillTask extends Leaf
 {
 
-    public ChasePowerPillTask(BTPacMan parent)
-    {
-        super(parent);
-    }
+	public ChasePowerPillTask(BTPacMan parent)
+	{
+		super(parent);
+	}
 
-    @Override
-    public boolean DoAction(Game game)
-    {
-        parent.setMove(game.getNextMoveTowardsTarget(
-                state.getCurrent(),
-                state.getNearestPowerPill(),
-                Constants.DM.PATH));
-        return true;
-    }
+	@Override
+	public boolean DoAction(Game game)
+	{
+		if (state.getNearestPowerPill() == -1)
+			return false;
+
+		parent.setMove(game.getNextMoveTowardsTarget(
+				state.getCurrent(),
+				state.getNearestPowerPill(),
+				Constants.DM.PATH));
+		return true;
+	}
 
 }
