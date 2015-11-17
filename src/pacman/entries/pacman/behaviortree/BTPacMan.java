3c754f0866e5a01834fa183201f1b3644f422fcd
salm <salm@itu.dk>
2015-11-17 16:34:40 +0100

    Added mutation

--- src/pacman/entries/pacman/behaviortree/BTPacMan.java
+++ src/pacman/entries/pacman/behaviortree/BTPacMan.java
@@ -3,8 +3,14 @@ package pacman.entries.pacman.behaviortree;
 import pacman.controllers.Controller;
 import pacman.entries.pacman.GameState;
 import pacman.entries.pacman.behaviortree.helpers.Composite;
+import pacman.entries.pacman.behaviortree.helpers.Leaf;
+import pacman.entries.pacman.behaviortree.helpers.Node;
 import pacman.game.Constants.MOVE;
 import pacman.game.Game;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class BTPacMan extends Controller<MOVE>
 		implements Comparable<BTPacMan>
@@ -40,6 +46,56 @@ public class BTPacMan extends Controller<MOVE>
 		// combine
 		throw new UnsupportedOperationException("Not implemented yet");
 	}
+
+    public static BTPacMan Mutate(BTPacMan parent){
+        BTPacMan mutatedParent = parent;
+
+        Random random = new Random();
+        int index = random.nextInt(mutatedParent.treeGenerator.getLeaves().size());
+        Node leaf = mutatedParent.treeGenerator.getLeaves().get(index);
+
+        if(leaf.type == Node.Type.Condition){
+            if(random.nextBoolean()){
+                for(int i = 0; i < leaf.parent.getChildrenCount(); i++){
+                    if(leaf.equals(leaf.parent.getChildren().get(i))){
+                        leaf.parent.getChildren().remove(i);
+                    }
+                }
+
+                mutatedParent.treeGenerator.getLeaves().remove(index);
+            }
+            else{
+                Leaf newLeaf = mutatedParent.treeGenerator.setOfConditions[random.nextInt(mutatedParent.treeGenerator.setOfConditions.length)];
+                while(newLeaf.equals(leaf)){
+                    newLeaf = mutatedParent.treeGenerator.setOfConditions[random.nextInt(mutatedParent.treeGenerator.setOfConditions.length)];
+                }
+
+                for(int i = 0; i < leaf.parent.getChildrenCount(); i++){
+                    if(leaf.equals(leaf.parent.getChildren().get(i))){
+                        leaf.parent.getChildren().set(i, newLeaf);
+                    }
+                }
+
+                mutatedParent.treeGenerator.getLeaves().set(index, newLeaf);
+            }
+        }
+        else{
+            Leaf newLeaf = mutatedParent.treeGenerator.setOfActions[random.nextInt(mutatedParent.treeGenerator.setOfActions.length)];
+            while(newLeaf.equals(leaf)){
+                newLeaf = mutatedParent.treeGenerator.setOfActions[random.nextInt(mutatedParent.treeGenerator.setOfActions.length)];
+            }
+
+            for(int i = 0; i < leaf.parent.getChildrenCount(); i++){
+                if(leaf.equals(leaf.parent.getChildren().get(i))){
+                    leaf.parent.getChildren().set(i, newLeaf);
+                }
+            }
+
+            mutatedParent.treeGenerator.getLeaves().set(index, newLeaf);
+        }
+
+        return mutatedParent;
+    }
 
 	@Override
 	public MOVE getMove(Game game, long timeDue)
